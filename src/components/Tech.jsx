c3067dcd76a31e82bafddb83bd26d38579d30bbf
git config --global user.name ArbaazN <arbaaznakhwa54@gmail.com>
2025-08-30 21:54:54 +0530

    Fix: show skills section on mobile

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -28,11 +28,11 @@ const Tech = () => {
       </div>
 
       {/* Mobile version */}
-      <div className="flex sm:hidden mt-10 flex-wrap justify-center gap-6">
+      <div className="flex sm:hidden mt-10 flex-wrap justify-center gap-6 bg-white p-6 rounded-2xl shadow-md">
         {technologies.map((technology) => (
           <div
             key={technology.name}
-            className="w-16 h-16 flex items-center justify-center bg-white/10 rounded-xl shadow-md"
+            className="w-16 h-16 flex items-center justify-center bg-gray-100 rounded-xl shadow"
           >
             <img
               src={technology.icon}
